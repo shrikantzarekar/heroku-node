9ed804bb817f1216a0f7d342d10c2cd600458eda
shrikantzarekar <shrikant.zarekar@gmail.com>
2016-11-12 23:44:26 +0530

    added initial code

---
+++ config.js
@@ -0,0 +1,28 @@
+var cfg = {};
+
+// HTTP Port to run our web application
+cfg.port = process.env.PORT || 3000;
+
+// A random string that will help generate secure one-time passwords and
+// HTTP sessions
+cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+// 
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = "AC2a41bb1019428857b769a44f3588e469";
+cfg.authToken = "8db3c7e7e1ce1fde9a89e2111613add0";
+
+// A Twilio number you control - choose one from:
+// https://www.twilio.com/user/account/phone-numbers/incoming
+// Specify in E.164 format, e.g. "+16519998877"
+cfg.twilioNumber = "+12674330454";
+
+// The sales rep / agent's phone number
+cfg.agentNumber = "+919010886000";
+
+// Export configuration object
+module.exports = cfg;
